8df85d0746eb2781ba2ea0ab88ec9fdab08e2698
acouch <acinternets@gmail.com>
2016-05-21 22:46:16 -0500

    Adds packages for cryptography for 14.04

--- roles/dkan_aws_ami/scripts/ansible.sh
+++ roles/dkan_aws_ami/scripts/ansible.sh
@@ -2,7 +2,7 @@
 
 # Update the box
 apt-get update
-apt-get -y install python-dev python-crypto python-pip
+apt-get -y install python-dev python-crypto python-pip libffi-dev libssl-dev
 pip install paramiko PyYAML Jinja2 httplib2 six
 pip install ansible
 ansible --version
