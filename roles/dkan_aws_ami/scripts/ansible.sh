e15237213926222285b74b94a46096f451ce9667
acouch <acinternets@gmail.com>
2016-05-22 23:12:52 -0500

    creates centos7 image

--- roles/dkan_aws_ami/scripts/ansible.sh
+++ roles/dkan_aws_ami/scripts/ansible.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-# Update the box
-apt-get update
-apt-get -y install python-dev python-crypto python-pip libffi-dev libssl-dev
+# Add Ansible on Debian
+#apt-get update
+#apt-get -y install python-dev python-crypto python-pip libffi-dev libssl-dev
+#pip install paramiko PyYAML Jinja2 httplib2 six
+#pip install ansible
+#ansible --version
+
+# Add Ansible on Centos
+# yum install python-setuptools python-setuptools-devel
+# easy_install pip
+
+yum -y install epel-release libffi-devel python-devel openssl-devel gcc
+yum -y install python-pip
 pip install paramiko PyYAML Jinja2 httplib2 six
 pip install ansible
 ansible --version
